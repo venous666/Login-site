1a7be80e7798ea2a482b9bb660cf54ca03fe4c8e
venus <99412095+venous666@users.noreply.github.com>
2022-11-25 09:58:26 -0300

    Add files via upload

---
+++ site.js
@@ -0,0 +1,30 @@
+gengar = []
+
+senha = document.getElementById('senha')
+email = documet.getElementById('email')
+botao  = document.getElementById('botao')
+
+function senha(){
+    gengar.push(senha)
+    console.log(senha)
+}
+
+function email(){
+    gengar.push(email)
+    console.log(email)
+}
+
+function botao(){
+
+    alert('dados cadastrados no site com sucesso!')
+    gengar.push(botao)
+    console.log(botao)
+}
+
+function botao(){
+    if (matricula>=45) {
+        alert('Aluno do Fundamental')
+    } else {
+        alert('Aluno do Ensino medio')
+    }
+}
